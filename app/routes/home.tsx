fda86c9d2d4b7237e122564a99139d60a04304c2
burhantaskin <112017727+burhantaskin@users.noreply.github.com>
2026-09-14 00:59:30 -0700

    home.tsx

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,27 @@
 import type { Route } from "./+types/home";
+
+import { RequireAuth } from "../components/auth/RequireAuth";
+import { MainNav } from "../components/navigation/MainNav";
 import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "WeTravel" },
-    { name: "description", content: "Welcome to WeTravel Website!" },
+    {
+      title: "WeTravel",
+    },
+    {
+      name: "description",
+      content: "Welcome to WeTravel!",
+    },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <RequireAuth>
+      <MainNav />
+
+      <Welcome />
+    </RequireAuth>
+  );
 }
