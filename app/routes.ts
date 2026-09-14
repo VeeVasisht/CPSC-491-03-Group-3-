e9c4650be3e2bd4d7d914e704de3349cb40d0427
burhantaskin <112017727+burhantaskin@users.noreply.github.com>
2026-09-14 16:57:19 -0700

    routes.ts

--- app/routes.ts
+++ app/routes.ts
@@ -1,6 +1,10 @@
-import { type RouteConfig, index, route } from "@react-router/dev/routes";
+import {
+  type RouteConfig,
+  index,
+  route,
+} from "@react-router/dev/routes";
 
 export default [
   index("routes/home.tsx"),
-  route("profile", "routes/profile.tsx"),
+  route("login", "routes/login.tsx"),
 ] satisfies RouteConfig;
