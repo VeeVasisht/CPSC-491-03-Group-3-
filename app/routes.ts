e7241483d7a9fefbd204d310f0c44f4789cf039b
burhantaskin <112017727+burhantaskin@users.noreply.github.com>
2026-09-14 19:14:56 -0700

    routes.ts

--- app/routes.ts
+++ app/routes.ts
@@ -1,8 +1,12 @@
-import { type RouteConfig, index, route } from "@react-router/dev/routes";
+import {
+  type RouteConfig,
+  index,
+  route,
+} from "@react-router/dev/routes";
 
 export default [
-    index("routes/home.tsx"),
-    route("login", "routes/login.tsx"),
-    route("forget-password", "../pages/ForgotPassword.jsx"),
-    route("settings", "../pages/AccountSettings.jsx"),
+  index("routes/home.tsx"),
+  route("login", "routes/login.tsx"),
+  route("forget-password", "../pages/ForgotPassword.jsx"),
+  route("settings", "../pages/AccountSettings.jsx"),
 ] satisfies RouteConfig;
